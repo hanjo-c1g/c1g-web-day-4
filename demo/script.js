5e30f4144d2c6894d6bb9a976c43b032efcbcb73
Hanjo Meinhardt <hanjo@bunix.de>
2025-01-31 12:08:36 +0100

    commit the updated demo code

--- demo/script.js
+++ demo/script.js
@@ -12,17 +12,68 @@ const doneCounter = document.getElementById("doneCounter");
 const undoneCounter = document.getElementById("undoneCounter");
 
 getButton.addEventListener( 'click', () => {
-    alert('not yet implemented');
+    fetch('https://jsonplaceholder.typicode.com/todos/2')
+        .then(response => {
+            if(!response.ok) {
+                throw new Error(`HTTP-Fehler: ${response.status}`)
+            }
+            return response.text()
+        })
+        .then(data => getResponse.textContent = data)
+        .catch(error => console.error("fehler: ", error ))
 });
 
 postButton.addEventListener( 'click', () => {
-    alert('not yet implemented');
-});
+    fetch('https://jsonplaceholder.typicode.com/todos/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify ({
+            title: 'Neues To-Do',
+            completed: false,
+            userId: 1
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP-Fehler: ${response.status}`);
+            }
+            return response.text();
+        })       
+        .then(data => postResponse.textContent = data)
+        .catch(error => console.error("fehler: ", error ))
+ });
 
 deleteButton.addEventListener( 'click', () => {
-    alert('not yet implemented');
+    fetch('https://jsonplaceholder.typicode.com/todos/1', {
+        method: 'DELETE'
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP-Fehler: ${response.status}`);
+          }
+          return response.text()
+        })
+        .then( data => deleteResponse.textContent = data )
+        .catch(error => console.error('Fehler beim Löschen:', error));
 });
 
 countButton.addEventListener( 'click', () => {
-  alert('not yet implemented');
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())        
+        .then(data => {
+        let done = 0;
+        let undone = 0;
+        data.forEach(element => {
+            if(element.completed) {
+                done++;
+            } else {
+                undone++;
+            }
+        });
+        doneCounter.textContent = done;
+        undoneCounter.textContent = undone;
+        } )
+        .catch(error => console.error('Fehler beim Abrufen:', error));  
 });
